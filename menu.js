d7a98cefd7e16e556cc44e52880a37068eb64652
SteveNerd22 <157782669+SteveNerd22@users.noreply.github.com>
2024-09-26 10:50:36 +0200

    Update menu.js

--- menu.js
+++ menu.js
@@ -2,5 +2,5 @@ function toggleMenu() {
     const sidebar = document.getElementById('sidebar');
     const bookmark = document.getElementById('bookmark');
     sidebar.classList.toggle('active');
-    bookmark.classList.toggle('active);
+    bookmark.classList.toggle('active');
 }
